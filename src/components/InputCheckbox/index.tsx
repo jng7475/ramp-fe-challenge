5f19a6d510b7f4fcafe8a1cdffc5803795f339e7
Trung Nguyen <trungthanh.nguyen@team.enouvo.com>
2024-08-23 14:19:59 -0500

    Modify bug 2

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -8,11 +8,11 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
+        htmlFor={inputId}
         className={classNames("RampInputCheckbox--label", {
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
         })}
-        onClick={() => onChange(!checked)}
       />
       <input
         id={inputId}
